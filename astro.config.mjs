b737cacd18869247e4cae38c8b63d023c5e28639
Derian Nazareno <derian.nazareno@hotmail.com>
2025-10-01 23:46:28 -0500

    añadiendo el canonical pagina de funteco

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://funteco.org',
+  site: 'https://www.fundaciontejiendoconocimiento.com/',
   integrations: [tailwind()],
   output: 'static',
 
