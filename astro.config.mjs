dc8628ef2300c31d81215ac3c0e16768ad4f7aac
Derian Nazareno <derian.nazareno@hotmail.com>
2025-10-10 14:48:02 -0500

    astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import node from '@astrojs/node'
 
 export default defineConfig({
   site: 'https://www.fundaciontejiendoconocimiento.com/',
   integrations: [tailwind()],
-  output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  output: 'static',
   style: { postcss: { plugins: [] } },
 })
